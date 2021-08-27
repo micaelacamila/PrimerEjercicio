26b9818100850d9d5453b21875cacedb4cdfe6dc
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-08-26 23:36:02 -0400

    Saludo segun hora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 const nombre=document.querySelector("#nombre-input");
 const edad=document.querySelector("#edad-input")
 const genero=document.querySelector("#genero-input")
+const hoy = new Date();
 const form=document.querySelector("#saludador-form");
 form.addEventListener("submit",(event)=>{
     gen="";
     mensaje = "Hola ";
-    if(edad.value > 30){
-        if(genero.value=="Mujer"){
-            gen="señora ";
+    if((hoy.getHours()>=5)&&(hoy.getHours()<12)) {
+        mensaje = mensaje+"buenos días, "
+    }
+    else{
+        if((hoy.getHours()>=12)&&(hoy.getHours()<19)){
+            mensaje=mensaje+"buenas tardes, "
         }
         else{
-            gen="señor "
+            mensaje=mensaje+"buenas noches, "
+        }
+    }
+    if(edad.value > 30){
+        if(genero.value=="Mujer"){
+            gen=" señora ";
+        }
+        else{
+            gen=" señor "
         }
     }else{
         if(genero.value=="Mujer"){
-            gen="señorita "
+            gen=" señorita "
         }
         else{
-            gen="joven "
+            gen=" joven "
         }
     }
-    alert(mensaje +gen+nombre.value);
+    alert(mensaje+gen+nombre.value);
 });
