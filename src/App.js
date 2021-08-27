0ee8b249c9bce60d4cb8d4b0f894f5b9c2b58bc8
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-08-26 23:19:37 -0400

    Edad y Genero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 const nombre=document.querySelector("#nombre-input");
-
+const edad=document.querySelector("#edad-input")
+const genero=document.querySelector("#genero-input")
 const form=document.querySelector("#saludador-form");
 form.addEventListener("submit",(event)=>{
+    gen="";
     mensaje = "Hola ";
-    alert(mensaje +nombre.value)
+    if(edad.value > 30){
+        if(genero.value=="Mujer"){
+            gen="señora ";
+        }
+        else{
+            gen="señor "
+        }
+    }else{
+        if(genero.value=="Mujer"){
+            gen="señorita "
+        }
+        else{
+            gen="joven "
+        }
+    }
+    alert(mensaje +gen+nombre.value);
 });
